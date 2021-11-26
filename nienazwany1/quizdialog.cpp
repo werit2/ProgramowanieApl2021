d3109f4b2bacc96fe6162d0ca55b16a6e5b7cfda
werit2 <73140150+werit2@users.noreply.github.com>
2021-11-26 14:03:54 +0100

    Add files via upload

--- nienazwany1/quizdialog.cpp
+++ nienazwany1/quizdialog.cpp
@@ -6,7 +6,9 @@ Quizdialog::Quizdialog(QWidget *parent) :
     ui(new Ui::Quizdialog)
 {
     ui->setupUi(this);
-    ui->textBrowser->setText("hello world");
+    ui-> label_3 -> setText("");
+    ui-> label_4 -> setText("");
+    w=l=0;
 
     QString filename  = QFileDialog::getOpenFileName(this, "Open", "", "Text files (*.txt)");
     QFile file(filename);
@@ -15,9 +17,15 @@ Quizdialog::Quizdialog(QWidget *parent) :
         return;
     }
     QTextStream in007(&file); //wczytywanie danych z pliku do zmiennej in007
-    lista = in007.readAll();
-
+    lista = in007.readAll(); //zmienna globalna dla calego qiuzdialog
     file.close();
+
+    QString text1,text2; //zadane pierwsze pytanie
+    text1 = lista.section('\n',l,l);
+    text2 = text1.section(':',0,0);
+    ui->textBrowser->setText(text2);
+    l++;
+
 }
 
 Quizdialog::~Quizdialog()
@@ -28,7 +36,25 @@ Quizdialog::~Quizdialog()
 
 void Quizdialog::on_pushButton_clicked()
 {
-    QString text1;
-    text1 = lista.section('\n',0,0);
-    ui->textBrowser->setText(text1);
+
+    QString text1,text2,text3;
+    text1 = lista.section('\n',l-1,l-1);
+    text2 = text1.section(':',1,1);
+    text3 = ui-> textEdit-> toPlainText();
+    if(text3 == text2) {
+        ui-> label_3 -> setText("Prawidłowa odpowiedź");
+        w++;
+    } else {
+        text3 = "Zła odpowiedź: " + text1;
+        ui-> label_3 -> setText( text3);
+    }
+    text1.setNum(w);
+    text2.setNum(l);
+    text3= "Poprawne odpowiedzi " + text1 + "/" + text2 ;
+    ui-> label_4 -> setText(text3); //to_string(l) + (char)w
+
+    text1 = lista.section('\n',l,l);
+    text2 = text1.section(':',0,0);
+    ui->textBrowser->setText(text2);
+    l++;
 }
